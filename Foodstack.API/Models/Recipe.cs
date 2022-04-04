1b058836c159e6d224b7128a9b514da48cfa24f4
Leon Jardevall <leonaccoun@gmail.com>
2022-04-04 16:47:06 +0200

    add jsonproperties to Recipe model

--- Foodstack.API/Models/Recipe.cs
+++ Foodstack.API/Models/Recipe.cs
@@ -1,23 +1,30 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Foodstack.API.Models;
 
 public class Recipe
 {
+  [JsonPropertyName("id")]
   public int Id { get; set; }
 
+  [JsonPropertyName("title")]
   [Required, MaxLength(127)]
   public string? Name { get; set; }
 
+  [JsonPropertyName("summary")]
   [Required, MaxLength(500)]
   public string? Description { get; set; }
 
+  [JsonPropertyName("image")]
   public string? ImageUrl { get; set; }
 
+  // [JsonPropertyName("")]
   [Range(0, 5)]
   public int Rating { get; set; }
 
   public string? Difficulty { get; set; }
 
+  [JsonPropertyName("readyInMinutes")]
   public int CookingTimeInMinutes { get; set; }
 }
